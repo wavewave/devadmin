c57aa8a89f4298c6b9bf14e1f8ae3f3991e320df
ianwookim <ianwookim@gmail.com>
2011-08-30 17:33:21 +0000

    use all dep ordered list for several jobs.

--- src/build.hs
+++ src/build.hs
@@ -26,6 +26,33 @@ import Application.DevAdmin.Job
 import Control.Applicative
 import Control.Monad
 
+
+makeProjDepOrderList :: BuildConfiguration -> IO (DaughterMap,[String])
+makeProjDepOrderList bc = do 
+  putStrLn $ show bc
+  let (p,w) = (,) <$> bc_progbase <*> bc_workspacebase $ bc
+  gdescs <- mapM (readPackageDescription normal . getCabalFileName (p,w) ) projects 
+  let deps = map ((,) <$> getPkgName <*> getDependency) gdescs
+      motherlist = map ((,) <$> fst  <*> (filter (nameMatch projects). snd)) deps
+      daughtermap = convertMotherMapToDaughterMap motherlist 
+      daughterlist = M.toList daughtermap 
+      edgelist = concatMap  mkDepEdge daughterlist
+      allnodes = idproj 
+      gr :: Gr String () 
+      gr = mkGraph allnodes edgelist
+      linear = topsort gr  
+      strlst = map (\x->fromJust $ M.lookup x idprojmap) linear 
+  return (daughtermap,strlst)
+
+makeProjDepList :: BuildConfiguration -> [String] -> IO [String]
+makeProjDepList bc projs = do 
+  (daughtermap,strlst) <- makeProjDepOrderList bc 
+     
+  let alldaughters = nub . concatMap (findAllDaughters daughtermap) $ projs
+      numbered = map (\x -> findOrder x strlst) alldaughters 
+      finallist = map snd . sortBy (compare `on` fst) $ numbered 
+  return finallist
+
 main :: IO ()
 main = do   
   args <- getArgs 
@@ -37,42 +64,26 @@ main = do
   let conf_result = parse configBuild "" configstr
   case conf_result of 
     Left err -> putStrLn (show err)
-    Right bc -> do 
-      putStrLn $ show bc
-      let (p,w) = (,) <$> bc_progbase <*> bc_workspacebase $ bc
-      gdescs <- mapM (readPackageDescription normal . getCabalFileName (p,w) ) projects 
-      let deps = map ((,) <$> getPkgName <*> getDependency) gdescs
-          motherlist = map ((,) <$> fst  <*> (filter (nameMatch projects). snd)) deps
-          daughtermap = convertMotherMapToDaughterMap motherlist 
-          daughterlist = M.toList daughtermap 
-          edgelist = concatMap  mkDepEdge daughterlist
-          allnodes = idproj 
-          gr :: Gr String () 
-          gr = mkGraph allnodes edgelist
-          linear = topsort gr  
-          strlst = map (\x->fromJust $ M.lookup x idprojmap) linear 
-     
-      let alldaughters = nub $ findAllDaughters daughtermap  (args !! 1)
-          numbered = map (\x -> findOrder x strlst) alldaughters 
-          finallist = map snd . sortBy (compare `on` fst) $ numbered 
-
-      let projlist = map projname projects    
-
-      --  putStrLn $ show gdescs -- deps -- gdescs --  motherlist -- finallist
-
+    Right bc -> do
+      let projlist = map projname projects 
+      finallist <- makeProjDepList bc [args !! 1]
+      allorderedlist <- makeProjDepList bc (map projname projects)
+--      putStrLn $ show projlist
+--      putStrLn $ show finallist
+--      putStrLn $ show allorderedlist 
 
       case (args !! 0) of 
         "install" -> flip mapM_ finallist (cabalInstallJob  bc)
-        "push"    -> flip mapM_ projlist  (darcsPushJob     bc)
+        "push"    -> flip mapM_ allorderedlist  (darcsPushJob     bc)
         "haddock" -> flip mapM_ finallist (haddockJob       bc)
         "depshow" -> flip mapM_ finallist (depshowJob       bc)
         "pull"    -> do 
-          putStrLn $ "projlist = " ++  (show projlist)
-          flip mapM_ projlist (darcsPullJob     bc)
+          putStrLn $ "allorderedlist = " ++  (show allorderedlist)
+          flip mapM_ allorderedlist (darcsPullJob     bc)
         "hoogle"  -> flip mapM_ finallist (hoogleJob        bc)
-        "whatsnew" -> flip mapM_ projlist (darcsWhatsnewJob bc)
-        "bootstrap" -> flip mapM_ projlist (cabalInstallJob bc)
-
+        "whatsnew" -> flip mapM_ allorderedlist (darcsWhatsnewJob bc)
+        "bootstrap" -> flip mapM_ allorderedlist (cabalInstallJob bc)
+        _ -> error "no such option"
 
 
     
